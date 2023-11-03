6c7297ec2e416bb5901c7e639312774ce3eff962
Deep-06 <shikhadeep.hzp@gmail.com>
2023-11-03 23:34:14 +0530

    Sidebar

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-
-export const ProductCard = ({id,image,name,price,category}) => {
-  const navigate=useNavigate();
+import { VStack, HStack, Text, Button, Image } from '@chakra-ui/react';
+export const ProductCard = ({ id, image, name, price, category }) => {
+  const navigate = useNavigate();
 
   return (
-    <div >
-      <img src={image} alt={name} width='100%'/>
-      <h3>Product: {name}</h3>
-      <h3>Price: {price}</h3>
-      <h3>Category: {category}</h3>
-      <button onClick={()=>navigate(`/singleproduct/${id}`)}>{name} Details</button>
-      </div>
-     
+    <HStack >
+      <Image src={image} alt={name} width='100%' />
+      <VStack >
+        <Text fontsize='xl'>Product: {name}</Text>
+        <Text fontsize='xl'>Price: {price}</Text>
+        <Text fontsize='xl'>Category: {category}</Text>
+        <Button onClick={() => navigate(`/singleproduct/${id}`)}>{name} Details</Button>
+      </VStack>
+    </HStack>
+
   )
 }
 
