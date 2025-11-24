0226253f8172ae24ea53f2bcb970804274cbc9c4
Deepshikha <130358363+Deep-06@users.noreply.github.com>
2025-11-24 12:34:55 +0000

    Updated homepage

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
-import { VStack, Text } from '@chakra-ui/react';
+import { VStack, Text, HStack } from '@chakra-ui/react';
 export const Sidebar = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [category, setCategory] = useState(searchParams.getAll("category") || []);
@@ -35,78 +35,78 @@ export const Sidebar = () => {
     }, [category, order])
 
     return (
-        <VStack spacing={2} >
+        <VStack spacing={2} alignItems="flex-start">
             <Text fontSize='2xl'>Filter by Category</Text>
-            <div>
+            <HStack>
                 <input type="checkbox" value={"fruit"}
                     onChange={handleCategory} checked={category.includes("fruit")}
                 />
                 <label >Fruits</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"vegetable"}
                     onChange={handleCategory} checked={category.includes("vegetable")}
                 />
                 <label >Vegetables</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"dairy"}
                     onChange={handleCategory} checked={category.includes("dairy")}
                 />
                 <label >Dairy</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"bakery"}
                     onChange={handleCategory} checked={category.includes("bakery")}
                 />
                 <label >Bakery</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"seafood"}
                     onChange={handleCategory} checked={category.includes("seafood")}
                 />
                 <label >Seafood</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"fish"}
                     onChange={handleCategory} checked={category.includes("fish")}
                 />
                 <label >Fish</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"chicken"}
                     onChange={handleCategory} checked={category.includes("chicken")}
                 />
                 <label >Chicken</label>
-            </div>
-            <div>
+            </HStack>
+            <HStack>
                 <input type="checkbox" value={"mutton"}
                     onChange={handleCategory} checked={category.includes("mutton")}
                 />
                 <label >Mutton</label>
-            </div>
+            </HStack>
             
 
             <Text fontSize='2xl'>Sort by price</Text>
-            <VStack onChange={handleOrder}>
-                <div>
-                    <input type="radio" name="order" value={"asc"}
+            <VStack onChange={handleOrder} alignItems="flex-start">
+                <HStack>
+                    <input type="radio" name="order" value={"asc"} 
                         defaultChecked={order === "asc"}
                     />
                     <label >Low to High</label>
-                </div>
-                <div>
+                </HStack>
+                <HStack>
                     <input type="radio" name="order" value={"desc"}
                         defaultChecked={order === "desc"}
                     />
                     <label >High to Low</label>
-                </div>
-                <div>
+                </HStack>
+                <HStack>
                     <input type="radio" name="order" value={""}
                         defaultChecked={order === ""}
                     />
                     <label >Reset</label>
-                </div>
+                </HStack>
             </VStack>
         </VStack>
 
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -9,41 +9,41 @@ export const ProductPage = () => {
     <div>
 
     <DIV_CON >
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%'>
+      <VStack p={0} borderRadius='30%'>
         <Image src='https://img.freepik.com/premium-photo/pyramid-healthy-food-front-view_23-2148256086.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=sph' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Fruits</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%' >
+      <VStack p={0} borderRadius='30%' >
         <Image src='https://img.freepik.com/free-vector/vegetables-realistic-composition-white-background-with-tomato-onion-sweet-hot-pepper-eggplant_1284-16242.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=sph' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Vegetables</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%'>
+      <VStack p={0} borderRadius='30%'>
         <Image src='https://img.freepik.com/free-vector/milk-natural-dairy-products-picnic_1284-32755.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=ais' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Dairy</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%'>
+      <VStack p={0} borderRadius='30%'>
         <Image src='https://img.freepik.com/premium-photo/assortment-fresh-baked-bread-white-transparent-background_827055-606.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=ais' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Bakery</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%' >
+      <VStack p={0} borderRadius='30%' >
         <Image src='https://img.freepik.com/free-photo/top-view-appetizing-seafood-mix-wooden-table_181624-42803.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=sph' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Seafood</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%' >
+      <VStack p={0} borderRadius='30%' >
         <Image src='https://img.freepik.com/premium-photo/raw-chicken-drumstick-spices-fly-cutting-board-copy-space_94255-3643.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=ais' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Chicken</Text>
       </VStack>
 
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%' >
+      <VStack p={0} borderRadius='30%' >
         <Image src='https://img.freepik.com/premium-photo/pile-meat-with-sprig-rosemary-it-with-white-background_435599-364.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=ais' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Mutton</Text>
       </VStack>
-      <VStack bgColor={'whitesmoke'} p={0} borderRadius='30%' >
+      <VStack p={0} borderRadius='30%' >
         <Image src='https://img.freepik.com/free-photo/top-view-delicious-seafood-arrangement_23-2148926757.jpg?size=626&ext=jpg&ga=GA1.1.270366329.1690118250&semt=ais' alt='' w='70%' borderRadius='50%'/>
         <Text fontSize="xl" fontWeight="extrabold">Fish</Text>
       </VStack>
